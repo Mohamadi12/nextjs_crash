ab08b3157964d38eed2a3a8ed55d81474a065a07
Nana Mohamadi <adilnana78@gmail.com>
2025-01-15 00:39:27 +0100

    Home Section

--- src/app/(rout)/page.tsx
+++ src/app/(rout)/page.tsx
@@ -1,12 +1,19 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 const Page = () => {
   return (
     <>
-      Home
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup <br /> Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
