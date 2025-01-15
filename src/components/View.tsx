54530de431176a3b1219b85f71ad02c5c3efa726
Nana Mohamadi <adilnana78@gmail.com>
2025-01-15 23:54:47 +0100

    Sanity Write Client

--- src/components/View.tsx
+++ src/components/View.tsx
@@ -1,32 +1,34 @@
-import React from 'react'
-import Ping from './Ping'
-import { client } from '@/sanity/lib/client'
-import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries';
-import { after } from 'next/server';
+import React from "react";
+import Ping from "./Ping";
+import { client } from "@/sanity/lib/client";
+import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { after } from "next/server";
+import { writeClient } from "@/sanity/lib/write-client";
 
-const View = async ({id}: {id: string}) => {
-    const { views: totalViews } = await client
+const View = async ({ id }: { id: string }) => {
+  const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id });
 
-    //TODO: Update the number of views
-//   after(
-//     async () =>
-//       await writeClient
-//         .patch(id)
-//         .set({ views: totalViews + 1 })
-//         .commit(),
-//   );
+  //TODO: Update the number of views after view
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+  );
+
   return (
     <div className="view-container">
-        <div className="absolute -top-2 -right-2">
-            <Ping />
-        </div>
-        <p className="view-text">
-            <span className="font-black">Views: {totalViews}</span>
-        </p>
+      <div className="absolute -top-2 -right-2">
+        <Ping />
+      </div>
+      <p className="view-text">
+        <span className="font-black">Views: {totalViews}</span>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default View
+export default View;
