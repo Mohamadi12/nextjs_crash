37408b6ff8083c65f00d2a36ab31047a7043ab57
Nana Mohamadi <adilnana78@gmail.com>
2025-01-15 01:52:10 +0100

    All Startups & Startup Card

--- src/app/(rout)/page.tsx
+++ src/app/(rout)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 import React from "react";
 
 const Page = async ({
@@ -6,6 +7,20 @@ const Page = async ({
 }: {
   searchParams: Promise<{ query?: string }>;
 }) => {
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: 'Nana' },
+      description: "This is a description",
+      image:
+        "https://www.rolandberger.com/img/Tiles/Tile-Content/Roland_Berger-24_2195_Humanoid_robots-ST.jpg",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
+
   const query = (await searchParams).query;
   return (
     <>
@@ -17,6 +32,22 @@ const Page = async ({
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
         </p>
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for ${query}` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType) =>(
+              <StartupCard key={post?._id} post={post}/>
+            ))
+          ):(
+            <p className="no-result">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
